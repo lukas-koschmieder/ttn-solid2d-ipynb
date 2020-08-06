cdf35a700ea64774a7fa6a3531c9725485333121
Lukas Koschmieder <lukas.koschmieder@rwth-aachen.de>
2020-08-06 14:40:03 +0200

    Refactor widget

--- ttn_solid2d_ipynb.py
+++ ttn_solid2d_ipynb.py
@@ -6,78 +6,86 @@ import json
 import requests
 import time
 from IPython.display import clear_output
-from ipywidgets import FloatText, FloatSlider
+from ipywidgets import Box, FloatSlider, FloatText, Output, VBox
 import numpy as np
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-class Widget():
-  def __init__(self, server):
+class Widget(Box):
+  def __init__(self, server, t=0.0, verbose=False):
     self.server = server
-    self.ui_request = FloatText(description="Request [s]", disabled=True)
-    self.ui_predict = FloatText(description="Prediction [s]", disabled=True)
-    self.ui_render = FloatText(description="Rendering  [s]", disabled=True)
-    self.ui_time = FloatSlider(description="Time [s]", min=0.0, max=1.0, 
-                               step=0.01, continuous_update=False)
+    self.request = FloatText(description="Request [s]", disabled=True)
+    self.predict = FloatText(description="Prediction [s]", disabled=True)
+    self.render = FloatText(description="Rendering  [s]", disabled=True)
+    self.time = FloatSlider(description="Time [s]", min=0.0, max=1.0, 
+                            step=0.01, value=t, continuous_update=False)
+    self.output = Output()
+    
+    ui = (self.time,)
+    if verbose:
+      ui = ui + (self.request, self.predict, self.render)
+    ui = ui + (self.output,)
 
+    super(Widget, self).__init__((VBox(ui),))
+
+    self.time.observe(self.update, names=['value'])
+    self.update({'new':t})
+
+  def imshow(self, u_grid, v_grid):
+    u = self.ax1.imshow(u_grid.T, interpolation='nearest', cmap=plt.cm.jet, 
+                        origin='lower', aspect='equal', vmin=-1, vmax=0)
+    v = self.ax2.imshow(v_grid.T, interpolation='nearest', cmap=plt.cm.jet, 
+                        origin='lower', aspect='equal', vmin=0, vmax=1)
+    return (u, v)
+ 
   def plot(self, x, y, u, v):
-    with self.ui_output:
-      x, y, u, v = np.array(x), np.array(y), np.array(u), np.array(v)
+    x, y, u, v = np.array(x), np.array(y), np.array(u), np.array(v)
 
-      x_coords, y_coords = np.meshgrid(y, x)
-      xy_coords = np.hstack((x_coords.flatten()[:,None], 
+    x_coords, y_coords = np.meshgrid(y, x)
+    xy_coords = np.hstack((x_coords.flatten()[:,None], 
                             y_coords.flatten()[:,None]))
-      
-      u_grid = griddata(xy_coords, u.flatten(), 
-                        (x_coords, y_coords), method='linear')
-      v_grid = griddata(xy_coords, v.flatten(), 
-                        (x_coords, y_coords), method='linear')
+    u_grid = griddata(xy_coords, u.flatten(), (x_coords, y_coords), 
+                      method='linear')
+    v_grid = griddata(xy_coords, v.flatten(), (x_coords, y_coords), 
+                      method='linear')
 
+    if not hasattr(self, 'fig'): # figure lazy init
+      plt.ioff()
       fig, (ax1, ax2) = plt.subplots(2)
-
+      self.fig, self.ax1, self.ax2 = fig, ax1, ax2
       ax1.set_title('Temperature')
-      h1 = ax1.imshow(u_grid.T, interpolation='nearest', cmap=plt.cm.jet, 
-                      origin='lower', aspect='equal', vmin=-1, vmax=0)
-      cax1 = make_axes_locatable(ax1).append_axes("right", size="5%", pad=0.1)
-      fig.colorbar(h1, cax=cax1)
-
       ax2.set_title('Solid fraction')
-      h2 = ax2.imshow(v_grid.T, interpolation='nearest', cmap=plt.cm.jet, 
-                      origin='lower', aspect='equal', vmin=0, vmax=1)
+      (i1, i2) = self.imshow(u_grid, v_grid)
+      cax1 = make_axes_locatable(ax1).append_axes("right", size="5%", pad=0.1)
       cax2 = make_axes_locatable(ax2).append_axes("right", size="5%", pad=0.1)
-      fig.colorbar(h2, cax=cax2)
-
+      fig.colorbar(i1, cax=cax1)
+      fig.colorbar(i2, cax=cax2)
       plt.tight_layout()
-      plt.show()
+    else: # figure update
+      self.imshow(u_grid, v_grid)
+    
+    clear_output(wait=True)
+    display(self.fig)
 
   def update(self, t):
-      with self.ui_output:
-          request_start = time.time()
-          try:
-            #respond = requests.get(self.server, data={ 't': t })
-            respond = requests.get("{}?t={}".format(self.server, t))
-          except:
-            print('ERROR: Failed to request ttn-solid2d-server: {}'
-                  .format(self.server))
-            raise
-          self.ui_request.value = str(time.time() - request_start)
-          render_start = time.time()
-          try:
-            out = json.loads(respond.content)
-          except:
-            print('ERROR: Failed to parse ttn-solid2d-server resonse: {}'
-                  .format(respond.content))
-            raise
-          self.ui_predict.value = str(out['predict_time'])
-          self.plot(out['x'], out['y'], out['u'], out['v'])
-          self.ui_render.value = str(time.time() - render_start)
-        
-  def display(self, verbose=False):
-    if verbose:
-      display(self.ui_request, self.ui_predict, self.ui_render)
-    if not hasattr(self, 'ui'):
-      self.ui = interactive(self.update, t=self.ui_time)
-      self.ui_output = self.ui.children[-1]
-    return self.ui
+    with self.output:
+      request_start = time.time()
+      try:
+        respond = requests.get("{}?t={}".format(self.server, t['new']))
+      except:
+        print('ERROR: Failed to request ttn-solid2d-server: {}'
+              .format(self.server))
+        raise
+      self.request.value = str(time.time() - request_start)
+      render_start = time.time()
+      try:
+        out = json.loads(respond.content)
+      except:
+        print('ERROR: Failed to parse ttn-solid2d-server resonse: {}'
+              .format(respond.content))
+        raise
+      self.predict.value = str(out['predict_time'])
+      self.plot(out['x'], out['y'], out['u'], out['v'])
+      self.render.value = str(time.time() - render_start)
